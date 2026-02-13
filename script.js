369de305db0b9cffc00b67df4537e4f86bbe3823
Ikariszn <herzoginios@gmail.com>
2026-02-13 15:03:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
-// Elements
+// Elements 
 const envelope = document.getElementById("envelope-container");
 const letter = document.getElementById("letter-container");
 const noBtn = document.querySelector(".no-btn");
 const yesBtn = document.querySelector(".btn[alt='Yes']");
 
 const title = document.getElementById("letter-title");
-const catImg = document.getElementById("letter-cat");
+const owlImg = document.getElementById("letter-owl");
 const buttons = document.getElementById("letter-buttons");
 const finalText = document.getElementById("final-text");
 
 // Click Envelope
-
 envelope.addEventListener("click", () => {
     envelope.style.display = "none";
     letter.style.display = "flex";
 
-    setTimeout( () => {
+    setTimeout(() => {
         document.querySelector(".letter-window").classList.add("open");
-    },50);
+    }, 50);
 });
 
-// Logic to move the NO btn
-
+// Move NO button
 noBtn.addEventListener("mouseover", () => {
     const min = 200;
     const max = 200;
@@ -36,37 +34,34 @@ noBtn.addEventListener("mouseover", () => {
     noBtn.style.transform = `translate(${moveX}px, ${moveY}px)`;
 });
 
-// Logic to make YES btn to grow
+// YES button growth logic
+let yesScale = 1;
 
-// let yesScale = 1;
+yesBtn.style.position = "relative";
+yesBtn.style.transformOrigin = "center center";
+yesBtn.style.transition = "transform 0.3s ease";
 
-// yesBtn.style.position = "relative"
-// yesBtn.style.transformOrigin = "center center";
-// yesBtn.style.transition = "transform 0.3s ease";
+noBtn.addEventListener("click", () => {
+    yesScale += 0.5; // grows gradually
 
-// noBtn.addEventListener("click", () => {
-//     yesScale += 2;
+    if (yesBtn.style.position !== "fixed") {
+        yesBtn.style.position = "fixed";
+        yesBtn.style.top = "50%";
+        yesBtn.style.left = "50%";
+        yesBtn.style.transform = `translate(-50%, -50%) scale(${yesScale})`;
+    } else {
+        yesBtn.style.transform = `translate(-50%, -50%) scale(${yesScale})`;
+    }
+});
 
-//     if (yesBtn.style.position !== "fixed") {
-//         yesBtn.style.position = "fixed";
-//         yesBtn.style.top = "50%";
-//         yesBtn.style.left = "50%";
-//         yesBtn.style.transform = `translate(-50%, -50%) scale(${yesScale})`;
-//     }else{
-//         yesBtn.style.transform = `translate(-50%, -50%) scale(${yesScale})`;
-//     }
-// });
-
-// YES is clicked
-
+// YES clicked
 yesBtn.addEventListener("click", () => {
-    title.textContent = "Yay :)!";
+    title.textContent = "Yay! :)";
 
-    catImg.src = "cat_dance.gif";
+    owlImg.src = "barnowl.png";
 
     document.querySelector(".letter-window").classList.add("final");
 
     buttons.style.display = "none";
-
     finalText.style.display = "block";
 });
